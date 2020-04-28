48cc576e1bd06a42b9736efaee6aca0bf365a71d
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-04-28 15:49:43 -0500

    Fix rubocop offenses.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
@@ -31,9 +30,9 @@ class UsersController < ApplicationController
     end
   end
 
-  private 
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
-  end 
-end 
+  end
+end
