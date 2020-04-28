ae2d9ce836b800cd73fbe914d2f4ed5284a7a618
flakster <carlos.santamaria@gmail.com>
2020-04-28 15:37:49 -0500

    Add edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to action: 'index'
     else
       render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to action: 'index'
+    else
+      render :edit
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
   get 'users/new'
   get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
 end
